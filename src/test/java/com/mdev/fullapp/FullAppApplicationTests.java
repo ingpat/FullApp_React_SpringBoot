b1f9b816d6f2e2174c67c2e786b00c892f9a81c3
ingpat <ingpatrickls.com>
2021-11-27 05:23:56 -0500

    first commit

---
+++ src/test/java/com/mdev/fullapp/FullAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdev.fullapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
